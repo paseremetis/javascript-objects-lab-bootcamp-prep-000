32fdaf39acaf6f6d0ccb946126d9953d1b006e9f
Peter Seremetis <pswriter5348@gmail.com>
2018-01-28 22:47:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {
+  prop: '1',
+  prop2: '2'
+};
+function updateObjectWithKeyAndValue(object, key, value){
+  return recipes
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  object = 'prop'
+  return object 
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  object = key
+  return object
+  if ('1' === 'undefined'); {
+  return object
+  }
+}
